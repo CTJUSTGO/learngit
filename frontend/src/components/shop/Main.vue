6df806a15ac6a2b95c4f414ab51c5a44e2f36976
woyaogaigeming <2219144609@qq.com>
2017-07-31 10:38:48 +0800

    aaaa

--- frontend/src/components/shop/Main.vue
+++ frontend/src/components/shop/Main.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <mt-navbar v-model="selected"> 
-      <mt-tab-item id="1">选项一</mt-tab-item>
-      <mt-tab-item id="2">选项二</mt-tab-item>
+      <mt-tab-item id="1">商品</mt-tab-item>
+      <mt-tab-item id="2">评价</mt-tab-item>
     </mt-navbar>
 
     <!-- tab-container -->
@@ -29,6 +29,20 @@
   }
 </script>
 
-<style>
-
+<style lang="scss">
+@import '../../../static/hotcss/px2rem.scss';
+  .mint-navbar{
+    .mint-tab-item{
+      padding: 0;
+      flex: 1;
+      display: block;
+      width: 0;
+      text-align: center;
+      .mint-tab-item-label{
+        height: px2rem(90);
+         font-size: px2rem(28);
+         line-height: px2rem(90);
+      }
+    }
+  }
 </style>
